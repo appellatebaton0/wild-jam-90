28221564be92677d761c116b1c6d0ba332d39503
Nic <130799100+SpazticSqurrel@users.noreply.github.com>
2026-02-14 00:57:44 -0600

    more character shading

---
+++ Assests/3D/Player Character/animated_character.gd
@@ -0,0 +1,30 @@
+@tool
+extends Node3D
+
+#INFO it's a tool script to see animation changes in real time
+
+@export var character_material: ShaderMaterial
+@onready var AnimTree = $AnimationTree as AnimationTree
+@export_enum("Idle", "Walk", "Sprint") var movement_state := "Idle":
+	set(to):
+		if to != movement_state:
+			AnimTree.set("parameters/Movement/transition_request", to)
+		movement_state = to
+
+var total_time := 0.0
+
+var tracked_position := self.global_position
+var fire_direction := Vector3.UP
+var fire_strength := 0.5
+
+func _process(delta: float):
+	total_time += delta
+	
+	tracked_position = lerp(tracked_position, self.global_position, 6.0 * delta);
+	fire_direction = lerp(fire_direction, to_local(tracked_position) + Vector3.UP, 12.0 * delta)
+	
+	fire_strength = 0.75 + (0.125 * sin(total_time * 1.0))
+	
+	if character_material:
+		character_material.set_shader_parameter("vertex_fire_direction", fire_direction)
+		character_material.set_shader_parameter("vertex_fire_strength", fire_strength)
