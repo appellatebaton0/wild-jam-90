9672c19e898c405bc5df0a60c3e7ccfde2e0f829
Nic <130799100+SpazticSqurrel@users.noreply.github.com>
2026-02-17 01:13:00 -0600

    linearize and standardize audio sliders

--- Scripts/Bits/AudioBusBit.gd
+++ Scripts/Bits/AudioBusBit.gd
@@ -2,17 +2,24 @@ class_name AudioBus extends Bit
 
 @export var bar:Range
 @export var label:Label
+@export var title:Label
 
 @export var bus_name:StringName
+var bus_idx := 0
 
 func _ready() -> void:
-	bar.value_changed.connect(_on_value_changed)
-	bar.value = bar.max_value / 2
-	_on_value_changed(bar.value)
+	_setup_audio_and_slider.call_deferred()
 
-func _on_value_changed(to:float): 
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus_name), to - (bar.max_value / 2))
-	
-	label.text = str(to - (bar.max_value / 2))
-	
-	
+func _setup_audio_and_slider():
+	title.text = bus_name
+	bus_idx = AudioServer.get_bus_index(bus_name)
+	bar.value_changed.connect(_on_value_changed)
+	var current_volume = AudioServer.get_bus_volume_linear(bus_idx)
+	current_volume -= fmod(current_volume, bar.step)
+	#print(bus_name, ", ", bus_idx, ", ", current_volume)
+	bar.value = current_volume
+	_on_value_changed(current_volume)
+
+func _on_value_changed(to:float):
+	AudioServer.set_bus_volume_linear(bus_idx, to)
+	label.text = str(to)
