4dd8b1e1879efcbd7e7d6df0c9339c39e444baac
Nic <130799100+SpazticSqurrel@users.noreply.github.com>
2026-02-18 17:25:05 -0600

    better suck, update crate dissolve

--- Scripts/Suck.gd
+++ Scripts/Suck.gd
@@ -1,10 +1,24 @@
 extends Area3D
 class_name SuckRange
 
-func _on_area_entered(area: CollisionObject3D) -> void:
-	var player = area.global_position
-	var tween = create_tween()
-	var collectable = $".."
-	
-	#print(" I see it")
-	tween.tween_property(collectable, "global_position", player, 0.5)
+## suction speed in m/s
+@export var suck_speed := 0.5
+## increase force as distance closes
+@export var suck_force := 15.0
+## distance in meters at which the suck gives up
+@export var suck_abandon_range := 15.0
+
+@onready var parent = $".."
+var tracking_node: Node3D
+
+func _physics_process(delta):
+	if tracking_node and is_instance_valid(parent):
+		var distance = (parent.global_position - tracking_node.global_position).length()
+		if distance <= suck_abandon_range:
+			var strength = (1.0 - (distance / suck_abandon_range)) * suck_force
+			parent.global_position = parent.global_position.move_toward(tracking_node.global_position, delta * (suck_speed + strength))
+		else:
+			tracking_node = null
+
+func _on_area_entered(area: Area3D) -> void:
+	tracking_node = area
