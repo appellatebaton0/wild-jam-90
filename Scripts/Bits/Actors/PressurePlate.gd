8ece2722c5862c981786a31ab2baa2c5f5835339
Nic <130799100+SpazticSqurrel@users.noreply.github.com>
2026-02-17 23:07:49 -0600

    pressure plate, misc terrain, fire fx

--- Scripts/Bits/Actors/PressurePlate.gd
+++ Scripts/Bits/Actors/PressurePlate.gd
@@ -1,9 +1,14 @@
 class_name PressurePlateBit extends Actor
-@onready var pressureplate_anim = $Pressure_Plate/AnimationPlayer
 
 ## How long the plate will stay down for after it's no longer activated.
 @export var weight_time := 0.0
 var weight_timer := 0.0
+
+@export var animation_player: AnimationPlayer
+@export var press_anim_name := "Press"
+@export var release_anim_name := "Release"
+
+var was_active := false
 
 var area:Area3D
 
@@ -19,16 +24,23 @@ func _ready() -> void:
 	for child in get_children():
 		if child is Area3D and not area: area = child
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var is_pressed = area.has_overlapping_bodies()
+	var is_active = is_pressed
 	
-	if area.has_overlapping_bodies(): weight_timer = weight_time
-	else: weight_timer = move_toward(weight_timer, 0, delta)
+	if is_pressed:
+		weight_timer = weight_time
+	else:
+		if weight_time > 0.0:
+			weight_timer = move_toward(weight_timer, 0.0, delta)
+			is_active = weight_timer > 0.0
 	
-	if area.has_overlapping_bodies(): pressureplate_anim.play("Held")
-	else: pressureplate_anim.play("IDK")
+	if animation_player:
+		if is_active and !was_active:
+			animation_player.play(press_anim_name, 0.1)
+		elif !is_active and was_active:
+			animation_player.play(release_anim_name, 0.1)
 	
-	
-	
-	value = weight_timer > 0 or area.has_overlapping_bodies()
+	was_active = is_active
+	value = is_active
