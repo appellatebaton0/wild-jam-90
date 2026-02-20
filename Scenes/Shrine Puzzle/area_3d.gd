3e78eb4b0a72b71d5757cc0856f64f10c21ee17f
Kyverii <120539424+Kyverii@users.noreply.github.com>
2026-02-19 22:12:46 -0500

    Puzzles added

---
+++ Scenes/Shrine Puzzle/area_3d.gd
@@ -0,0 +1,13 @@
+extends Area3D
+@export var reset:Node
+
+
+func _on_area_entered(area: Detector) -> void:
+	var copy = reset.duplicate()
+	print("Duplicated")
+	reset.queue_free()
+	get_parent().add_child(copy)
+	reset = copy
+	
+	
+	
